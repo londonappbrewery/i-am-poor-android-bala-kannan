d2f12696dcbbbf68cb9004e8b41c0266d6d7577e
bala-kannan <bala_kannan@hotmail.com>
2017-10-02 17:48:09 -0700

    Challenge completed

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -3,12 +3,18 @@ package com.londonappbrewery.iampoor;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import static android.graphics.Paint.UNDERLINE_TEXT_FLAG;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mPaintFlags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
